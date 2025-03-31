7c8a23f668157479481fa7dc5995dd9117e7f6ed
tuka0304 <phido0002@gmail.com>
2025-03-31 17:02:35 +0700

    Update PhongKhoaBan.java

--- lab5/bai5/PhongKhoaBan.java
+++ lab5/bai5/PhongKhoaBan.java
@@ -1,3 +1,44 @@
-public class PhongKhoaBan {
+import java.util.Scanner;
+class PhongKhoaBan {
+    private String maPhongKhoaBan;
+    private String tenPhongKhoaBan;
 
+    public PhongKhoaBan() {
+        this.maPhongKhoaBan = "";
+        this.tenPhongKhoaBan = "";
+    }
+
+    public PhongKhoaBan(String maPhongKhoaBan, String tenPhongKhoaBan) {
+        this.maPhongKhoaBan = maPhongKhoaBan;
+        this.tenPhongKhoaBan = tenPhongKhoaBan;
+    }
+
+    public String getMaPhongKhoaBan() {
+        return maPhongKhoaBan;
+    }
+
+    public void setMaPhongKhoaBan(String maPhongKhoaBan) {
+        this.maPhongKhoaBan = maPhongKhoaBan;
+    }
+
+    public String getTenPhongKhoaBan() {
+        return tenPhongKhoaBan;
+    }
+
+    public void setTenPhongKhoaBan(String tenPhongKhoaBan) {
+        this.tenPhongKhoaBan = tenPhongKhoaBan;
+    }
+
+    public void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap ma phong khoa ban: ");
+        this.maPhongKhoaBan = scanner.nextLine();
+        System.out.print("Nhap ten phong khoa ban: ");
+        this.tenPhongKhoaBan = scanner.nextLine();
+    }
+
+    public void xuat() {
+        System.out.println("Ma phong khoa ban: " + maPhongKhoaBan);
+        System.out.println("Ten phong khoa ban: " + tenPhongKhoaBan);
+    }
 }
