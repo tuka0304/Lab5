a82da56962938c2055de04e70079da3960778a8e
tuka0304 <phido0002@gmail.com>
2025-03-31 17:03:08 +0700

    Update Main_bai5.java

--- lab5/bai5/Main_bai5.java
+++ lab5/bai5/Main_bai5.java
@@ -1,3 +1,13 @@
-public class Main_bai5 {
+import java.util.Date;
+public class Main_bai5 {public static void main(String[] args) {
+    PhongKhoaBan phongKhoaBan = new PhongKhoaBan("PK01", "Khoa CNTT");
+    NhanVien nhanVien = new NhanVien("Nguyen Thi Lan", 1990, 10000000, new Date(), phongKhoaBan);
 
+    nhanVien.nhap();
+
+    System.out.println("\nThong tin nhan vien:");
+    nhanVien.xuat();
 }
+}
+
+
